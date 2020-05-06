39f6c0dfab53ecc7b24b317746143c8d8a9bcc44
DiegoBravo123 <55288027+DiegoBravo123@users.noreply.github.com>
2020-05-06 17:40:46 -0500

    Controllers ya eliminan

---
+++ PersonalController.java
@@ -0,0 +1,126 @@
+package pe.edu.upc.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pe.edu.upc.entity.Personal;
+import pe.edu.upc.entity.Rol;
+import pe.edu.upc.service.IPersonalService;
+import pe.edu.upc.service.IRolService;
+
+@Named
+@RequestScoped
+public class PersonalController implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private IPersonalService peS;
+	@Inject
+	private IRolService rS;
+	
+	private Personal personal;
+	private List<Personal> listaPersonal;
+	private Rol rol;
+	private List<Rol> listaRol;
+	
+	@PostConstruct
+	public void init()
+	{
+		this.personal= new Personal();
+		this.rol= new Rol();
+		this.listaPersonal = new ArrayList<Personal>();
+		this.listarPersonal();
+		this.listarRol();
+	}
+
+	public Personal getPersonal() {
+		return personal;
+	}
+
+	public void setPersonal(Personal personal) {
+		this.personal = personal;
+	}
+
+	public List<Personal> getListaPersonal() {
+		return listaPersonal;
+	}
+
+	public void setListaPersonal(List<Personal> listaPersonal) {
+		this.listaPersonal = listaPersonal;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public List<Rol> getListaRol() {
+		return listaRol;
+	}
+
+	public void setListaRol(List<Rol> listaRol) {
+		this.listaRol = listaRol;
+	}
+	
+	public void listarPersonal() {
+		
+		try {
+			this.listaPersonal=peS.listar();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+	
+     public void listarRol() {
+		
+		try {
+			this.listaRol=rS.listar();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+     
+     public String nuevoPersonal()
+     {
+    	 this.setPersonal(new Personal());
+    	 return "personal.xhtml";
+     }
+     
+     public void insertarPersonal()
+     {
+    	 try {
+    		 peS.insertar(this.personal);
+    		 limpiarPersonal();
+    		 listarPersonal();		
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+     }
+     
+     public void limpiarPersonal()
+     {
+    	 this.init();
+     }
+     
+ 	public void eliminar(int idPersonal) {
+		try {
+			peS.eliminar(idPersonal);
+			listarPersonal();
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+}
---
+++ RolController.java
@@ -0,0 +1,84 @@
+package pe.edu.upc.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pe.edu.upc.entity.Rol;
+import pe.edu.upc.service.IRolService;
+
+@Named
+@RequestScoped
+public class RolController {
+	@Inject
+	private IRolService rS;
+	private Rol rol;
+	List<Rol> listaRoles;
+	
+	@PostConstruct
+	public void init() {
+		this.listaRoles = new ArrayList<Rol>();
+		this.rol = new Rol();
+		this.listar();
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public List<Rol> getListaRoles() {
+		return listaRoles;
+	}
+
+	public void setListaRoles(List<Rol> listaRoles) {
+		this.listaRoles = listaRoles;
+	}
+	
+	public String nuevoRol() {
+		this.setRol(new Rol());
+		return "rol.xhtml"; 
+	}
+	
+	public void listar() {
+		try {
+			listaRoles = rS.listar();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+
+
+	public void insertar() {
+		try {
+			rS.Insertar(rol);
+			limpiarRol();
+			this.listar();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+	public void limpiarRol() {
+		this.init();
+
+	}
+	
+	public void eliminar(int idRol) {
+		try {
+			rS.eliminar(idRol);
+			listar();
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+}
